1452429d2044c70eef655f2fbbea4d4a42933f48
Wang Bai <wangbai1981@gmail.com>
2013-07-24 10:23:09 +0800

    renew PoplygonWindow

--- src/com/javadocmd/simplelatlng/window/PolygonWindow.java
+++ src/com/javadocmd/simplelatlng/window/PolygonWindow.java
@@ -1,5 +1,6 @@
 package com.javadocmd.simplelatlng.window;
 
+
 import com.javadocmd.simplelatlng.LatLng;
 import com.javadocmd.simplelatlng.util.LatLngConfig;
 
@@ -45,217 +46,51 @@ public class PolygonWindow extends LatLngWindow<PolygonWindow> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public int containsBound(LatLng point) {
-		// TODO Auto-generated method stub
-		int total = this.points.length;
-		int count = 0;
-		LatLng pointOne = this.points[0];
-
-		for (int i = 1; i <= total; i++) {
-			// point is an vertex
-			if ((point.getLongitude() == pointOne.getLongitude())
-					&& (point.getLatitude() == pointOne.getLatitude())) {
-				return 2;
-			}
-
-			// right vertex
-			LatLng pointTwo = this.points[i % total];
-			// ray is outside of our interests
-			if ((point.getLatitude() < min(pointOne.getLatitude(),
-					pointTwo.getLatitude()))
-					|| (point.getLatitude() > max(pointOne.getLatitude(),
-							pointTwo.getLatitude()))) {
-				// next ray left point
-				pointOne.setLatitudeLongitude(pointTwo.getLatitude(), pointTwo.getLongitude());
-			
-				continue;
-			}
-
-			// ray is crossing over by the algorithm (common part of)
-			if ((point.getLatitude() > min(pointOne.getLatitude(),
-					pointTwo.getLatitude()))
-					&& (point.getLatitude() < max(pointOne.getLatitude(),
-							pointTwo.getLatitude()))) {
-				// x is before of ray
-				if (point.getLongitude() <= max(pointOne.getLongitude(),
-						pointTwo.getLongitude())) {
-					// overlies on a horizontal ray
-					if ((pointOne.getLatitude() == pointTwo.getLatitude())
-							&& (point.getLongitude() >= min(
-									pointOne.getLongitude(),
-									pointTwo.getLongitude()))) {
-						return 2;
-					}
-					// ray is vertical
-					if (pointOne.getLongitude() == pointTwo.getLongitude()) {
-						// overlies on a ray
-						if (pointOne.getLongitude() == point.getLongitude()) {
-							return 2;
-						} else {// before ray
-							++count;
-						}
-
-					} else {// cross point on the left side
-						// cross point of x
-						double xinters = ((point.getLatitude() - pointOne
-								.getLatitude()) * (pointTwo.getLongitude() - pointOne
-								.getLongitude()))
-								/ (pointTwo.getLatitude() - pointOne
-										.getLatitude())
-								+ pointOne.getLongitude();
-						// overlies on a ray
-						if (Math.abs(point.getLongitude() - xinters) < LatLngConfig.DEGREE_TOLERANCE) {
-							return 2;
-						}
-						// before ray
-						if (point.getLongitude() < xinters) {
-							++count;
-						}
-					}
-				}
-			} else {// special case when ray is crossing through the vertex
-					// p crossing over p2
-				if ((point.getLatitude() == pointTwo.getLatitude())
-						&& (point.getLongitude() <= pointTwo.getLongitude())) {
-					// next vertex
-					LatLng pointThree = this.points[(i + 1) % total];
-					// p.y lies between p1.y & p3.y
-					if ((point.getLatitude() >= min(pointOne.getLatitude(),
-							pointThree.getLatitude()))
-							&& (point.getLatitude() <= max(
-									pointOne.getLatitude(),
-									pointThree.getLatitude()))) {
-						++count;
-					} else {
-						count += 2;
-					}
-				}
-			}
-
-			// next ray left point
-			pointOne.setLatitudeLongitude(pointTwo.getLatitude(), pointTwo.getLongitude()) ;
-		}
-
-		if ((count % 2) == 0) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-
 	
 	@Override
-	public boolean contains(LatLng point) {
-		// TODO Auto-generated method stub
-		int total = this.points.length;
-		int count = 0;
-		LatLng pointOne = this.points[0];
-
-		for (int i = 1; i <= total; i++) {
-			// point is an vertex
-			if ((point.getLongitude() == pointOne.getLongitude())
-					&& (point.getLatitude() == pointOne.getLatitude())) {
-				return true;
-			}
-			// right vertex
-			LatLng pointTwo = this.points[i % total];
-			// ray is outside of our interests
-			if ((point.getLatitude() < min(pointOne.getLatitude(),
-					pointTwo.getLatitude()))
-					|| (point.getLatitude() > max(pointOne.getLatitude(),
-							pointTwo.getLatitude()))) {
-				// next ray left point
-				pointOne.setLatitudeLongitude(pointTwo.getLatitude(), pointTwo.getLongitude());
-			
-				continue;
-			}
-
-			// ray is crossing over by the algorithm (common part of)
-			if ((point.getLatitude() > min(pointOne.getLatitude(),
-					pointTwo.getLatitude()))
-					&& (point.getLatitude() < max(pointOne.getLatitude(),
-							pointTwo.getLatitude()))) {
-				// x is before of ray
-				if (point.getLongitude() <= max(pointOne.getLongitude(),
-						pointTwo.getLongitude())) {
-					// overlies on a horizontal ray
-					if ((pointOne.getLatitude() == pointTwo.getLatitude())
-							&& (point.getLongitude() >= min(
-									pointOne.getLongitude(),
-									pointTwo.getLongitude()))) {
-						return true;
-					}
-					// ray is vertical
-					if (pointOne.getLongitude() == pointTwo.getLongitude()) {
-						// overlies on a ray
-						if (pointOne.getLongitude() == point.getLongitude()) {
-							return true;
-						} else {// before ray
-							++count;
-						}
-
-					} else {// cross point on the left side
-						// cross point of x
-						double xinters = ((point.getLatitude() - pointOne
-								.getLatitude()) * (pointTwo.getLongitude() - pointOne
-								.getLongitude()))
-								/ (pointTwo.getLatitude() - pointOne
-										.getLatitude())
-								+ pointOne.getLongitude();
-						// overlies on a ray
-						if (Math.abs(point.getLongitude() - xinters) < LatLngConfig.DEGREE_TOLERANCE) {
-							return true;
-						}
-						// before ray
-						if (point.getLongitude() < xinters) {
-							++count;
-						}
-					}
-				}
-			} else {// special case when ray is crossing through the vertex
-					// p crossing over p2
-				if ((point.getLatitude() == pointTwo.getLatitude())
-						&& (point.getLongitude() <= pointTwo.getLongitude())) {
-					// next vertex
-					LatLng pointThree = this.points[(i + 1) % total];
-					// p.y lies between p1.y & p3.y
-					if ((point.getLatitude() >= min(pointOne.getLatitude(),
-							pointThree.getLatitude()))
-							&& (point.getLatitude() <= max(
-									pointOne.getLatitude(),
-									pointThree.getLatitude()))) {
-						++count;
-					} else {
-						count += 2;
-					}
+	public boolean contains(LatLng p) {
+		int total = points.length;
+		int through = 0;
+		
+		for (int i = 0, j = i + 1; i < total; ++i, j = i + 1) {
+			LatLng p1 = points[i];
+			LatLng p2 = points[j%total];
+	
+			/*
+			 * calculate the point number, 
+			 * when p radiate a leftward ray paralleling with y axis 
+			 */
+			if ( (p.getLatitude() > p1.getLatitude()) && (p.getLatitude() <= p2.getLatitude()) 
+				|| (p.getLatitude() > p2.getLatitude()) && (p.getLatitude() <= p1.getLatitude()) ) {		
+				if ( isLeft(p1, p2, p) ) {
+					++through;
 				}
 			}
-
-			// next ray left point
-			pointOne.setLatitudeLongitude(pointTwo.getLatitude(), pointTwo.getLongitude()) ;
 		}
-
-		if ((count % 2) == 0) {
+		  
+		if (through % 2 == 0) {
 			return false;
-		} else {
-			return true;
 		}
+		
+		return true;
 	}
 
+	public static boolean isLeft(LatLng p1, LatLng p2, LatLng p) {
+		double judgeValueX = ( p1.getLongitude() - p2.getLongitude()) 
+							/ (p1.getLatitude() - p2.getLatitude()) 
+							* (p.getLatitude() - p2.getLatitude() ) 
+							+ p2.getLongitude();
+		
+		if (judgeValueX > p.getLongitude()) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 	@Override
 	public boolean overlaps(PolygonWindow window) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	private static double max(double first, double second) {
-		return (first > second) ? first : second;
-	}
-
-	private static double min(double first, double second) {
-		return (first < second) ? first : second;
-	}
-
 }
--- test/com/javadocmd/simplelatlng/window/PolygonWindowTest.java
+++ test/com/javadocmd/simplelatlng/window/PolygonWindowTest.java
@@ -17,12 +17,13 @@ public class PolygonWindowTest {
 		// TODO Auto-generated method stub
 		LatLng[] points = new LatLng[4];
 		points[0] = new LatLng(0,0);
-		points[1] = new LatLng(0,1);
+		points[1] = new LatLng(1,0);
 		points[2] = new LatLng(1,1);
-		points[3] = new LatLng(1,0);
+		points[3] = new LatLng(0,1);
+		
 		PolygonWindow polygon = new PolygonWindow(points);
 		
-		System.out.println(polygon.contains(new LatLng(0, 0)));
+		System.out.println(polygon.contains(new LatLng(0.3, 0.4)));
 	}
 
 }
